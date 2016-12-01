595c83630abefaace92351a49afd776632470fef
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-11-30 19:19:27 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  new_pigeon_list = {}
+  pigeon_data.each do |data_type, value_hash|
+    value_hash.each do |data_value, pigeon_list|
+      pigeon_list.each do |pigeon_name|
+        if new_pigeon_list.include?(pigeon_name)
+          if new_pigeon_list[pigeon_name].include?(data_type)
+            new_pigeon_list[pigeon_name][data_type].push(data_value.to_s)
+          else
+            new_pigeon_list[pigeon_name][data_type] = [data_value.to_s]
+          end
+        else
+          new_pigeon_list[pigeon_name]= {}
+          new_pigeon_list[pigeon_name][data_type] = [data_value.to_s]
+        end
+      end
+    end
+  end
+  new_pigeon_list
 end
