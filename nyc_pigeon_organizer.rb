e0e49fc77420472343ed0ffcb8dfd7a35fe6c8ae
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-03 14:38:01 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,34 @@
+require 'pry'
+
 def nyc_pigeon_organizer(pigeon_data)
-  new_pigeon_list = {}
+  new_pigeon_list = Hash.new { |pigeon_name, data_type| pigeon_name[data_type] = Hash.new { |data_type, data_value| data_type[data_value] = Array.new } }
   pigeon_data.each do |data_type, value_hash|
     value_hash.each do |data_value, pigeon_list|
       pigeon_list.each do |pigeon_name|
-        if new_pigeon_list.include?(pigeon_name)
-          if new_pigeon_list[pigeon_name].include?(data_type)
-            new_pigeon_list[pigeon_name][data_type].push(data_value.to_s)
-          else
-            new_pigeon_list[pigeon_name][data_type] = [data_value.to_s]
-          end
-        else
-          new_pigeon_list[pigeon_name]= {}
-          new_pigeon_list[pigeon_name][data_type] = [data_value.to_s]
-        end
+        new_pigeon_list[pigeon_name][data_type] << data_value.to_s
       end
     end
   end
   new_pigeon_list
 end
+
+# def nyc_pigeon_organizer(pigeon_data)
+#   new_pigeon_list = {}
+#   pigeon_data.each do |data_type, value_hash|
+#     value_hash.each do |data_value, pigeon_list|
+#       pigeon_list.each do |pigeon_name|
+#         if new_pigeon_list.include?(pigeon_name)
+#           if new_pigeon_list[pigeon_name].include?(data_type)
+#             new_pigeon_list[pigeon_name][data_type].push(data_value.to_s)
+#           else
+#             new_pigeon_list[pigeon_name][data_type] = [data_value.to_s]
+#           end
+#         else
+#           new_pigeon_list[pigeon_name]= {}
+#           new_pigeon_list[pigeon_name][data_type] = [data_value.to_s]
+#         end
+#       end
+#     end
+#   end
+#   new_pigeon_list
+# end
